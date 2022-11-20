69579121c81fc8f2163261cf8a1de90058ddd9f9
Eric Fei <guanwha@gmail.com>
2022-11-21 00:51:06 +0800

    [Deploy] deploy to github page -- fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/f2e2022-week2-signer/"
+      : "./",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
